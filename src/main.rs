3897f6a19a942951ade6172674cdd909432a8be9
Serhii Pryvala <sog@Serhiis-Laptop.local>
2024-06-23 07:25:38 +0300

    Reconfig

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::startup::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
+    let port = listener.local_addr().unwrap().port();
+    println!("Working on {} port", port);
+    run(listener)?.await
 }
